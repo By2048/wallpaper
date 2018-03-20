5582336ecd7ec9bf2f4190693d0844a5fb0712d3
By2048 <1247079575@qq.com>
2018-03-20 20:47:06 +0800

    用户表细节修改

--- app/user/models.py
+++ app/user/models.py
@@ -29,26 +29,26 @@ class UserProfile(AbstractUser):
     image
     """
     _sex = (
-        ('1', '男'),
-        ('0', '女'),
-        ('-1', '中'),
+        (1, '男'),
+        (0, '女'),
+        (-1, '中'),
     )
     _type = (
-        ('1', '普通用户 '),
-        ('2', '会员'),
-        ('3', '作者')
+        (1, '普通用户'),
+        (2, '会员'),
+        (3, '作者')
     )
 
     nickname = models.CharField(max_length=50, blank=True, null=True, verbose_name='昵称')
     image = models.ImageField(upload_to='resource/user_image', default='/resource/user_image/default.png',
                               null=True, blank=True, verbose_name='头像')
-    sex = models.CharField(max_length=10, choices=_sex, default='中', null=True, blank=True, verbose_name='性别')
+    sex = models.IntegerField(choices=_sex, default=-1, null=True, blank=True, verbose_name='性别')
     birthday = models.DateField(max_length=10, null=True, blank=True, verbose_name='生日')
     address = models.CharField(max_length=100, null=True, blank=True, verbose_name='地址')
     phone = models.CharField(max_length=11, null=True, blank=True, verbose_name='手机号')
     information = models.TextField(null=True, blank=True, verbose_name='信息')
     last_login = models.DateTimeField(default=timezone.now, blank=True, null=True, verbose_name='最后登录时间')
-    type = models.CharField(max_length=50, choices=_type, default='0', verbose_name='用户类型', blank=True, null=True)
+    type = models.IntegerField(choices=_type, default=1, verbose_name='用户类型', blank=True, null=True)
     coin = models.IntegerField(default=100, verbose_name='用户积分')
 
     def __str__(self):
@@ -117,7 +117,6 @@ class UserAuthentication(models.Model):
         ('0', '未激活'),
         ('1', '已激活'),
     )
-    id = models.AutoField(primary_key=True, verbose_name='ID')
     verification_code = models.CharField(max_length=50, verbose_name='验证码')
     email = models.EmailField(max_length=50, verbose_name='邮箱')
     send_type = models.CharField(choices=_types, max_length=10, verbose_name='验证类型')
